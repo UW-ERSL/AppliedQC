670898698f316abc334ed5d0a82f483d15ba7868
UW-ERSL <ksuresh@wisc.edu>
2026-01-19 08:29:39 -0600

    BV Problem added

--- Chapter02_QuantumSoftware_functions.py
+++ Chapter02_QuantumSoftware_functions.py
@@ -1,1 +1,13 @@
 
+
+## Berstein-Vazirani function
+
+def secretBitStringFunction(x):
+    s = '11010'
+    if (len(x) != len(s)):
+        print('Error: Length of secret string is', len(s))
+        return 0
+    a = 0
+    for i in range(len(s)):
+        a = a + int(s[i])*x[i]
+    return a%2
